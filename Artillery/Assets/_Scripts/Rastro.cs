b37b2c031ae9f042e53db1db5a480be4675157c2
franciscasofia1211-gif <franciscasofia1211@gmail.com>
2026-09-14 13:01:17 -0300

    Entrega Practica 17
    Se realiza lo solicitado para el encargo

---
+++ Artillery/Assets/_Scripts/Rastro.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+    
+public class Rastro : MonoBehaviour
+{
+    [Header("configurar en editor")]
+    public float ditanciaMinimaEntrePuntos = 0.1f;
+    private LineRenderer linea;
+    private GameObject _objetivoLinea;
+    private List<Vector3> puntos;
+    public GameObject objetivoLinea
+    {
+        get { return (_objetivoLinea); }
+        set
+        {
+            _objetivoLinea = value;
+            if (_objetivoLinea != null)
+            {
+                linea.enabled = false;
+                puntos = new List<Vector3>();
+                AgregarPunto();
+            }
+        }
+    }
+    public Vector3 ultimoPunto
+    {
+        get
+        {
+            if (puntos == null)
+            {
+                return (Vector3.zero);
+            }
+            return (puntos[puntos.Count - 1]);
+        }
+    }
+
+    public void AgregarPunto()
+    {
+        Vector3 punto = _objetivoLinea.transform.position;
+        if (puntos.Count > 0 && (punto - ultimoPunto).magnitude < ditanciaMinimaEntrePuntos)
+        {
+            return;
+        }
+        puntos.Add(punto);
+        linea.positionCount = puntos.Count;
+        linea.SetPosition(puntos.Count - 1, ultimoPunto);
+        linea.enabled = true;
+    }
+    private void Awake()
+    {
+        linea = GetComponent<LineRenderer>();
+        linea.enabled = false;
+        puntos = new List<Vector3>();
+    }
+    private void FixedUpdate()
+    {
+        if (_objetivoLinea == null)
+        {
+            if (SeguirCamara.objetivo != null)
+            {
+                if (SeguirCamara.objetivo.tag == "Bala")
+                {
+                    objetivoLinea = SeguirCamara.objetivo;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        AgregarPunto();
+        if (SeguirCamara.objetivo == null)
+        {
+            objetivoLinea = null;
+        }
+    }
+}
