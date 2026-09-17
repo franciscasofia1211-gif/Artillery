f3a8fd8f8e64ef117a8ccd73dd847368b3b9557d
franciscasofia1211-gif <franciscasofia1211@gmail.com>
2026-09-17 16:53:54 -0300

    Revert "Practica Modulo 18"

--- Artillery/Assets/_Scripts/Canon.cs
+++ Artillery/Assets/_Scripts/Canon.cs
@@ -2,50 +2,40 @@ using UnityEngine;
 
 public class Canon : MonoBehaviour
 {
-    public static bool Bloqueado;
-    public AudioClip ClipDisparo;
-    private GameObject SonidoDisparo;
-    private AudioSource SourceDisparo;
-    public GameObject ParticulaDisparo;
     [SerializeField] private GameObject BalaPrefab;
-    [SerializeField] private GameObject puntaCanon;
+    [SerializeField]private GameObject puntaCanon;
     private float rotacion;
 
     private void Start()
     {
         puntaCanon = transform.Find("PuntaCanon").gameObject;
-        SonidoDisparo = GameObject.Find("SonidoDisparo");
-        SourceDisparo = SonidoDisparo.GetComponent<AudioSource>();
     }
     private void Update()
     {
         rotacion += Input.GetAxis("Horizontal") * AdministradorJuego.VelocidadRotacion;
         if (rotacion <= 90 && rotacion >= 0)
         {
-            transform.eulerAngles = new Vector3(0, 0, rotacion);
+            transform.eulerAngles = new Vector3(0,0,rotacion);
         }
         if (rotacion > 90)
         {
             rotacion = 90;
         }
-        if (rotacion < 0)
+        if(rotacion < 0)
         {
             rotacion = 0;
         }
-        if (Input.GetKeyDown(KeyCode.Space) && !Bloqueado)
+        if (AdministradorJuego.DisparoPorJuego > 0) 
         {
-            AdministradorJuego.DisparoPorJuego--;
-            GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
-            SeguirCamara.objetivo = temp;
-            Rigidbody tempRB = temp.GetComponent<Rigidbody>();
-            Vector3 DireccionDisparo = new Vector3(-rotacion, 0,0);
-            DireccionDisparo.y = 90 + DireccionDisparo.x;
-            Vector3 direccionParticulas = new Vector3(-90 + DireccionDisparo.x, 90,0);
-            GameObject Particulas = Instantiate(ParticulaDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
-            tempRB.linearVelocity = DireccionDisparo.normalized * AdministradorJuego.VelocidadBala;
-            SourceDisparo.Play();
-            Bloqueado = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                AdministradorJuego.DisparoPorJuego--;
+                GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+                Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+                Vector3 DireccionDisparo = transform.rotation.eulerAngles;
+                DireccionDisparo.x = -90 + DireccionDisparo.y;
+                tempRB.linearVelocity = DireccionDisparo.normalized * AdministradorJuego.VelocidadBala;
+            } 
         }
-
     }
 }
